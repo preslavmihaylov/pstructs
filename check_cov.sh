bd37567ff1005ec2bddf28a263fdcd5198f68a53
PreslavMihaylov <preslav@pmihaylov.com>
2019-01-26 17:31:50 +0200

    refactored check_cov script

--- check_cov.sh
+++ check_cov.sh
@@ -5,11 +5,11 @@ TOTAL_PERCENT=$(cat coverage/report_raw.txt | grep TOTAL.* | awk '{print $4}')
 TOTAL_PERCENT=${TOTAL_PERCENT::-1}
 EXIT_CODE=0
 
-if [ "$TOTAL_PERCENT" -lt $TARGET_PERCENT ]; then
-    echo "FAILED: Coverage Test"
-    EXIT_CODE=1
-else
+if [ "$TOTAL_PERCENT" -gt $TARGET_PERCENT ]; then
     echo "PASSED: Coverage Test"
+    EXIT_CODE=0
+else
+    echo "FAILED: Coverage Test"
     EXIT_CODE=1
 fi
 
