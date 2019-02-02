8cbc0696c527d32141e6143c45dec4aa9da4b1f5
PreslavMihaylov <preslav@pmihaylov.com>
2019-02-02 10:50:16 +0200

    added setBit, raiseBit, clearBit to bit_ops interface

--- src/bit_ops/tests/test_bit_ops.cpp
+++ src/bit_ops/tests/test_bit_ops.cpp
@@ -15,7 +15,15 @@ CPPUNIT_TEST_SUITE(BitOpsTest);
 CPPUNIT_TEST(test_bitsCntOne);
 CPPUNIT_TEST(test_bitsCntFifteen);
 CPPUNIT_TEST(test_bitsCntMax);
-//CPPUNIT_TEST_EXCEPTION(test_popEmpty, std::invalid_argument);
+CPPUNIT_TEST(test_clearBitChangeExpected);
+CPPUNIT_TEST(test_clearBitNoChangeExpected);
+CPPUNIT_TEST(test_clearBitBigNumber);
+CPPUNIT_TEST(test_raiseBitChange);
+CPPUNIT_TEST(test_raiseBitNoChange);
+CPPUNIT_TEST(test_raiseBitBigNumber);
+CPPUNIT_TEST(test_setBitBigNumber);
+CPPUNIT_TEST(test_setBitZero);
+CPPUNIT_TEST(test_setBitOne);
 
 CPPUNIT_TEST_SUITE_END();
 
@@ -36,6 +44,60 @@ public:
     {
         int cnt = pstructs::bit_ops::bitsCnt((uint32_t)-1);
         CPPUNIT_ASSERT(cnt == 32);
+    }
+
+    void test_clearBitChangeExpected()
+    {
+        int res = pstructs::bit_ops::clearBit(15, 0);
+        CPPUNIT_ASSERT(res == 14);
+    }
+
+    void test_clearBitNoChangeExpected()
+    {
+        int res = pstructs::bit_ops::clearBit(14, 0);
+        CPPUNIT_ASSERT(res == 14);
+    }
+
+    void test_clearBitBigNumber()
+    {
+        int res = pstructs::bit_ops::clearBit(268402688, 15);
+        CPPUNIT_ASSERT(res == 268369920);
+    }
+
+    void test_raiseBitChange()
+    {
+        int res = pstructs::bit_ops::raiseBit(14, 0);
+        CPPUNIT_ASSERT(res == 15);
+    }
+
+    void test_raiseBitNoChange()
+    {
+        int res = pstructs::bit_ops::raiseBit(15, 1);
+        CPPUNIT_ASSERT(res == 15);
+    }
+
+    void test_raiseBitBigNumber()
+    {
+        int res = pstructs::bit_ops::raiseBit(268369920, 15);
+        CPPUNIT_ASSERT(res == 268402688);
+    }
+
+    void test_setBitBigNumber()
+    {
+        int res = pstructs::bit_ops::setBit(268369920, 15, 1);
+        CPPUNIT_ASSERT(res == 268402688);
+    }
+
+    void test_setBitZero()
+    {
+        int res = pstructs::bit_ops::setBit(15, 1, 0);
+        CPPUNIT_ASSERT(res == 13);
+    }
+
+    void test_setBitOne()
+    {
+        int res = pstructs::bit_ops::setBit(13, 1, 1);
+        CPPUNIT_ASSERT(res == 15);
     }
 };
 
